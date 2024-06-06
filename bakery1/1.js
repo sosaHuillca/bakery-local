48cd68ef1eab0168ff2b0c6cf2add7a512cf714a
sosahuillca <sandex7@protonmail.com>
2024-06-06 18:58:09 -0500

    ordenando directorio

--- bakery1/1.js
+++
@@ -1,8 +0,0 @@
-let nodo = item => document.querySelector(item)
-
- nodo(".content-galery").childNodes.forEach(imagen => {
-   imagen.addEventListener("click",function(){
-      let url = this.getAttribute("src")
-      nodo(".head-product img").setAttribute("src",url)
-   })
-})
